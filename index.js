de6ff4e4d50c8771e654e3634cdcbcd15b62dbf3
githubMYL <githubMYL@163.com>
2019-01-13 21:51:58 +0800

    update

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 window.onload = function () {
     var el = document.getElementById("editor");
     // var initValue = "class Solution:\n\tdef func:\n\t\tpass";
-    var initValue = "1 + 2";
+	var version = "# version: Python3\n\n";
+	var codeAreaTip = "# please edit your code here:\n";
+	var codeStart = "# code start\n\n";
+	var codeEnd = "# code end\n\n";
+	var codeTip = "'''\nThis function is the entry of this program and\nit must be return your answer of current question.\n'''\n";
+	var code = "def solution():\n\tpass";
+    var initValue = version + codeAreaTip + codeStart + codeEnd + codeTip + code;
     var myCodeMirror = CodeMirror.fromTextArea(el, {
         mode: "python",
         theme: "leetcode",
@@ -20,7 +26,7 @@ window.onload = function () {
     });
     myCodeMirror.setOption("value", initValue);
     myCodeMirror.on("keypress", function() {
-        myCodeMirror.showHint(); // 注意，注释了show-hint.js第131行的代码（阻止了代码补全，同时提供智能提示）
+        myCodeMirror.showHint(); // 注意，注释了CodeMirror库中show-hint.js第131行的代码（阻止了代码补全，同时提供智能提示）
     });
     var test = document.getElementById("test");
     test.onclick = function() {
